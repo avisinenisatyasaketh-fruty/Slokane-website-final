b90c97c49fc902809c2dab291d68eff1ff7fc3a6
JanakiRam Uppalapati <janakiramuppalapati@JanakiRams-MacBook-Pro.local>
2026-03-25 00:21:43 +0530

    final fix database url

--- slokane-website/backend/db.js
+++ slokane-website/backend/db.js
@@ -1,15 +1,6 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,   // ✅ FIXED
-  database: process.env.DB_NAME,       // ✅ FIXED
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
   if (err) {
